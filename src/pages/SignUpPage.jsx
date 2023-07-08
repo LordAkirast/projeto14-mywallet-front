a3d4fb91ee5eb690e2ba32f5c24f3f8f1578d6b9
LordAkirast <darkwarish@gmail.com>
2023-07-08 02:51:49 -0300

    Configurado a parte de cadastro de novo usuário. Alerts colocados com sucesso.

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,30 +1,88 @@
-import { Link } from "react-router-dom"
-import styled from "styled-components"
-import MyWalletLogo from "../components/MyWalletLogo"
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import MyWalletLogo from "../components/MyWalletLogo";
+import axios from "axios";
+import { useState } from 'react';
 
 export default function SignUpPage() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [confirmarSenha, setConfirmarSenha] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = {
+      nome,
+      email,
+      senha,
+      confirmarSenha,
+    };
+
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/cadastro`, data);
+      console.log(response.data); // Trate a resposta da API conforme necessário
+      setNome("")
+      setEmail("")
+      setSenha("")
+      setConfirmarSenha("")
+    
+  
+    
+    } catch (error) {
+      if (!nome) {
+        alert("Nome precisa ser preenchido!")
+      }
+
+      if (!email) {
+        alert("Email precisa ser preenchido!")
+      }
+
+      if (!senha) {
+        alert("Senha precisa ser preenchido!")
+      }
+
+      if (!confirmarSenha) {
+        alert("Senha precisa ser preenchido!")
+      }
+
+      if (senha.length < 3) {
+        alert("Senha precisa ter mais de 3 caracteres!")
+      }
+
+      if (senha !== confirmarSenha) {
+        alert("A senha e a confirmação de senha precisam ser iguais!")
+        console.error(422);
+      }
+
+
+      console.error(error);
+    }
+  };
+
   return (
-    <SingUpContainer>
-      <form>
+    <SignUpContainer>
+      <form onSubmit={handleSubmit}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input placeholder="Nome" type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
+        <input placeholder="E-mail" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input placeholder="Senha" type="password" autoComplete="new-password" value={senha} onChange={(e) => setSenha(e.target.value)} />
+        <input placeholder="Confirme a senha" type="password" autoComplete="new-password" value={confirmarSenha} onChange={(e) => setConfirmarSenha(e.target.value)} />
+        <button type="submit">Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to="/">
         Já tem uma conta? Entre agora!
       </Link>
-    </SingUpContainer>
-  )
+    </SignUpContainer>
+  );
 }
 
-const SingUpContainer = styled.section`
+const SignUpContainer = styled.section`
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
